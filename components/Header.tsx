9d0748c1678385ac081b6db48fb982b7a0cb11ea
Jatin Sonkusale <jatin.s@zebpay.com>
2024-08-29 00:27:07 +0530

    wallet connection with chain switching

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
-"use client"; 
+"use client";
 
 import Link from "next/link";
 import { IoWallet } from "react-icons/io5";
+import WalletConnect from "./WalletConnect";
+import { useNetworkMismatch } from "@thirdweb-dev/react";
+
 
 export default function Header() {
+  const isMismatch = useNetworkMismatch();
   return (
-    <header className="sticky top-0 z-50 bg-white bg-opacity-70 backdrop-blur-lg rounded-full shadow-md w-[33vw]">
+    <header className={`sticky top-0 z-50 bg-white bg-opacity-70 backdrop-blur-lg rounded-full shadow-md w-[33vw] border-2 ${isMismatch?"border-red-500":"border-green-500"}`}>
       <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div>
@@ -15,8 +19,7 @@ export default function Header() {
           </div>
           <div>
             <div className="cursor-pointer">
-              <div className="w-2 h-2 bg-amber-500 rounded-full"></div>
-              <IoWallet size={"32px"} />
+              <WalletConnect />
             </div>
           </div>
         </div>
